ece6eeb472f7cfdc682453177793a44d34ae7410
Daniel Filgueira <dfsdireito95@gmail.com>
2020-07-29 14:30:36 -0300

    adicionando material-ui e ajustando estilos com material-ui

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,16 +1,87 @@
-import styled from 'styled-components'
+import { makeStyles } from "@material-ui/core/styles";
+import { cor } from "../../global/colors.json";
 
-export const Wrapper = styled.section`
-  width: 100vw;
-  height: 100vh;
-`
+export const useStyles = makeStyles({
+  container: {
+    background: cor.branca,
+    color: "white",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
 
-export const Header = styled.header`
-  width: 100%;
-  height: 100%;
-  background-color: #7971EA;
-  font-size: 14px;
-`
-export const PanelContent = styled.div`
+  header: {
+    background: cor.tom5,
+    color: "white",
+    width: "100vw",
+    height: "40px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-`
+  contentBox: {
+    background: cor.tom3,
+    width: "90%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: "20px",
+    marginBottom: "20px",
+    padding: "10px",
+    borderRadius: "10px",
+  },
+
+  titleContentBox: {
+    background: cor.tom5,
+    width: "100%",
+    height: "40px",
+    borderRadius: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  cards: {
+    background: cor.branca,
+    width: "100%",
+    height: "100%",
+    margin: "10px",
+    borderRadius: "10px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+  },
+
+  card: {
+    maxWidth: 200,
+    maxHeight: 240,
+    margin: "20px",
+  },
+
+  infosCard: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "left",
+    justifyContent: "flex-start",
+  },
+
+  buttonsCard: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  button: {
+    height: "40px",
+  }
+});
--- src/global/styles.ts
+++ src/global/styles.ts
@@ -4,18 +4,17 @@ const globalStyles = createGlobalStyle`
 
   /* removendo a formatacao padrao*/
   * {
+    border: 0;
     margin: 0;
     padding: 0;
     box-sizing: border-box;    
   }
 
-  /* inserindo o font-size da tag HTML para ajuste em responsividade */
-  html{
-    font-size: 62.5%;
-  }
+  /* inserindo o font-size da tag HTML para ajuste em responsividade */ 
 
   html, body{
-    height: 500%;
+    width: 100%;
+    height: 100%;
   }
 
   /* inserindo no corpo fontes padroes de cada sistema para melhorar o aspecto visual*/
